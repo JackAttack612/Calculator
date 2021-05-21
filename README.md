f26a4a43470b6b594d3ad1ffc78e79cde9e81624
JackAttack612 <jetflyer1130@gmail.com>
2021-05-21 12:24:11 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -3,11 +3,14 @@
  
 # How to use
 
-Select either 2 or 3, this is how many numbers you are using
+Select either 2, 3 or 4, this is how many numbers you are using
 start by puttig ONE number in and clicking enter when done
 put the operation number in (+, -, /, *)
 put the second number in
 (if using 3 numbers) put the second operation in
 put the third number in
-Repeat
+(if using 4 numbers) put in the third operation
+put in the fourth number
+repeat
+
 
--- calculator.py
+++ calculator.py
@@ -1,7 +1,7 @@
 print("\nUse +, -, *, or / as the operator")
 print('This calculator uses order of operation')
 
-option = input("\nChoose ammount of variables (2/3): ")
+option = input("\nChoose ammount of variables (2/3/4): ")
 
 if option == "2":
     num1 = float(input("\nEnter first number: "))
@@ -58,28 +58,167 @@ if option == "3":
         print(num12 * num22 - num32)
     elif op2 == "*" and op3 == "/":
         print(num12 * num22 / num32)
+
+if option == "4":
+    num13 = float(input("\nEnter first number: "))
+    op4 = input("Enter operator: ")
+    num23 = float(input("Enter second number: "))
+    op5 = input("Enter second operator: ")
+    num33 = float(input("Enter third number: "))
+    op6 = input("Enter Third operation: ")
+    num43 = float(input("Enter fourth number: "))
+
+    if op4 == "+" and op5 == "+" and op6 == "+":
+        print(num13 + num23 + num33 + num43)
+    elif op4 == "+" and op5 == "+" and op6 == "-":
+        print(num13 + num23 + num33 - num43)
+    elif op4 == "+" and op5 == "+" and op6 == "/":
+        print(num13 + num23 + num33 / num43)
+    elif op4 == "+" and op5 == "+" and op6 == "*":
+        print(num13 + num23 + num33 * num43)
+    elif op4 =="+" and op5 =="-" and op6 == "+":
+        print(num13 + num23 - num33 + num43)
+    elif op4 =="+" and op5 =="-" and op6 == "-":
+        print(num13 + num23 - num33 - num43)
+    elif op4 =="+" and op5 =="-" and op6 == "/":
+        print(num13 + num23 - num33 / num43)
+    elif op4 =="+" and op5 =="-" and op6 == "*":
+        print(num13 + num23 - num33 * num43)
+    elif op4 =="-" and op5 =="+" and op6 == "+":
+        print(num13 - num23 + num33 + num43)
+    elif op4 =="-" and op5 =="+" and op6 == "-":
+        print(num13 - num23 + num33 - num43)
+    elif op4 =="-" and op5 =="+" and op6 == "/":
+        print(num13 - num23 + num33 / num43)
+    elif op4 =="-" and op5 =="+" and op6 == "*":
+        print(num13 - num23 + num33 * num43)
+    elif op4 =="-" and op5 =="-" and op6 == "+":
+     print(num13 - num23 - num33 + num43)
+    elif op4 =="-" and op5 =="-" and op6 == "-":
+        print(num13 - num23 - num33 - num43)
+    elif op4 =="-" and op5 =="-" and op6 == "/":
+        print(num13 - num23 - num33 / num43)
+    elif op4 =="-" and op5 =="-" and op6 == "*":
+        print(num13 - num23 - num33 * num43)
+    elif op4 =="/" and op5 =="+" and op6 == "+":
+        print(num13 / num23 + num33 + num43)
+    elif op4 =="/" and op5 =="+" and op6 == "-":
+        print(num13 / num23 + num33 - num43)
+    elif op4 =="/" and op5 =="+" and op6 == "/":
+        print(num13 / num23 + num33 / num43)
+    elif op4 =="/" and op5 =="+" and op6 == "*":
+        print(num13 / num23 + num33 * num43)
+    elif op4 =="/" and op5 =="-" and op6 == "+":
+        print(num13 / num23 - num33 + num43)
+    elif op4 =="/" and op5 =="-" and op6 == "-":
+        print(num13 / num23 - num33 - num43)
+    elif op4 =="/" and op5 =="-" and op6 == "/":
+        print(num13 / num23 - num33 / num43)
+    elif op4 =="/" and op5 =="-" and op6 == "*":
+        print(num13 / num23 - num33 * num43)
+    elif op4 =="*" and op5 =="+" and op6 == "+":
+        print(num13 * num23 + num33 + num43)
+    elif op4 =="*" and op5 =="+" and op6 == "-":
+        print(num13 * num23 + num33 - num43)
+    elif op4 =="*" and op5 =="+" and op6 == "/":
+        print(num13 * num23 + num33 / num43)
+    elif op4 =="*" and op5 =="+" and op6 == "*":
+        print(num13 * num23 + num33 * num43)
+    elif op4 =="*" and op5 =="-" and op6 == "+":
+        print(num13 * num23 - num33 + num43)
+    elif op4 =="*" and op5 =="-" and op6 == "-":
+        print(num13 * num23 - num33 - num43)
+    elif op4 =="*" and op5 =="-" and op6 == "/":
+        print(num13 * num23 - num33 / num43)
+    elif op4 =="*" and op5 =="-" and op6 == "*":
+        print(num13 * num23 - num33 * num43)
+    elif op4 =="+" and op5 =="/" and op6 == "+":
+        print(num13 + num23 / num33 + num43)
+    elif op4 =="+" and op5 =="/" and op6 == "-":
+        print(num13 + num23 + num33 - num43)
+    elif op4 =="+" and op5 =="/" and op6 == "/":
+        print(num13 + num23 / num33 / num43)
+    elif op4 =="+" and op5 =="/" and op6 == "*":
+        print(num13 + num23 / num33 * num43)
+    elif op4 =="+" and op5 =="*" and op6 == "+":
+        print(num13 + num23 * num33 + num43)
+    elif op4 =="+" and op5 =="*" and op6 == "-":
+        print(num13 + num23 * num33 - num43)
+    elif op4 =="+" and op5 =="*" and op6 == "/":
+        print(num13 + num23 - num33 / num43)
+    elif op4 =="+" and op5 =="*" and op6 == "*":
+        print(num13 + num23 - num33 * num43)
+    elif op4 =="-" and op5 =="/" and op6 == "+":
+        print(num13 - num23 / num33 + num43)
+    elif op4 =="-" and op5 =="/" and op6 == "-":
+        print(num13 - num23 / num33 - num43)
+    elif op4 =="-" and op5 =="/" and op6 == "/":
+        print(num13 - num23 / num33 / num43)
+    elif op4 =="-" and op5 =="/" and op6 == "*":
+        print(num13 - num23 / num33 * num43)
+    elif op4 =="-" and op5 =="*" and op6 == "+":
+        print(num13 - num23 * num33 + num43)
+    elif op4 =="-" and op5 =="*" and op6 == "-":
+        print(num13 - num23 * num33 - num43)
+    elif op4 =="-" and op5 =="*" and op6 == "/":
+        print(num13 - num23 * num33 / num43)
+    elif op4 =="-" and op5 =="*" and op6 == "*":
+        print(num13 - num23 * num33 * num43)
+    elif op4 =="/" and op5 =="/" and op6 == "+":
+        print(num13 / num23 / num33 + num43)
+    elif op4 =="/" and op5 =="/" and op6 == "-":
+        print(num13 / num23 / num33 - num43)
+    elif op4 =="/" and op5 =="/" and op6 == "/":
+        print(num13 / num23 / num33 / num43)
+    elif op4 =="/" and op5 =="/" and op6 == "*":
+        print(num13 / num23 / num33 * num43)
+    elif op4 =="/" and op5 =="*" and op6 == "+":
+        print(num13 / num23 * num33 + num43)
+    elif op4 =="/" and op5 =="*" and op6 == "-":
+        print(num13 / num23 * num33 - num43)
+    elif op4 =="/" and op5 =="*" and op6 == "/":
+        print(num13 / num23 - num33 / num43)
+    elif op4 =="/" and op5 =="*" and op6 == "*":
+        print(num13 / num23 * num33 * num43)
+    elif op4 =="*" and op5 =="/" and op6 == "+":
+        print(num13 * num23 / num33 + num43)
+    elif op4 =="*" and op5 =="/" and op6 == "-":
+        print(num13 * num23 / num33 - num43)
+    elif op4 =="*" and op5 =="/" and op6 == "/":
+        print(num13 * num23 / num33 / num43)
+    elif op4 =="*" and op5 =="/" and op6 == "*":
+        print(num13 * num23 / num33 * num43)
+    elif op4 =="*" and op5 =="*" and op6 == "+":
+        print(num13 * num23 * num33 + num43)
+    elif op4 =="*" and op5 =="*" and op6 == "-":
+        print(num13 * num23 * num33 - num43)
+    elif op4 =="*" and op5 =="*" and op6 == "/":
+        print(num13 * num23 * num33 / num43)
+    elif op4 =="*" and op5 =="*" and op6 == "*":
+        print(num13 * num23 * num33 * num43)
+
     
-    cont = input("Press Enter to continue: ")
+cont = input("Press Enter to continue: ")
 
 
 while cont <= "":
-    option = input("\nChoose ammount of variables (2/3): ")
+    option = input("\nChoose ammount of variables (2/3/4): ")
 
     if option == "2":
         num1 = float(input("\nEnter first number: "))
         op = input("Enter operator: ")
         num2 = float(input("Enter second number: "))
 
-    if op == "+":
-        print(num1 + num2)
-    elif op == "-":
-        print(num1 - num2)
-    elif op == "/":
-        print(num1 / num2)
-    elif op == "*":
-        print(num1 * num2)
-    else:
-        print("Invalid operator. Please use +, -, /, or *")
+        if op == "+":
+            print(num1 + num2)
+        elif op == "-":
+            print(num1 - num2)
+        elif op == "/":
+            print(num1 / num2)
+        elif op == "*":
+            print(num1 * num2)
+        else:
+            print("Invalid operator. Please use +, -, /, or *")
 
     if option == "3":
         num12 = float(input("\nEnter first number: "))
@@ -89,7 +228,7 @@ while cont <= "":
         num32 = float(input("Enter third number: "))
 
         if op2 and op3 == "+":
-         print(num12 + num22 + num32)
+            print(num12 + num22 + num32)
         elif op2 == "+" and op3 == "-":
             print(num12 + num22 - num32)
         elif op2 == "+" and op3 == "*":
@@ -120,5 +259,144 @@ while cont <= "":
             print(num12 * num22 - num32)
         elif op2 == "*" and op3 == "/":
             print(num12 * num22 / num32)
+
+    if option == "4":
+        num13 = float(input("\nEnter first number: "))
+        op4 = input("Enter operator: ")
+        num23 = float(input("Enter second number: "))
+        op5 = input("Enter second operator: ")
+        num33 = float(input("Enter third number: "))
+        op6 = input("Enter Third operation: ")
+        num43 = float(input("Enter fourth number: "))
+
+        if op4 =="+" and op5 =="+" and op6 == "+":
+            print(num13 + num23 + num33 + num43)
+        elif op4 =="+" and op5 =="+" and op6 == "-":
+            print(num13 + num23 + num33 - num43)
+        elif op4 =="+" and op5 =="+" and op6 == "/":
+            print(num13 + num23 + num33 / num43)
+        elif op4 =="+" and op5 =="+" and op6 == "*":
+            print(num13 + num23 + num33 * num43)
+        elif op4 =="+" and op5 =="-" and op6 == "+":
+            print(num13 + num23 - num33 + num43)
+        elif op4 =="+" and op5 =="-" and op6 == "-":
+            print(num13 + num23 - num33 - num43)
+        elif op4 =="+" and op5 =="-" and op6 == "/":
+            print(num13 + num23 - num33 / num43)
+        elif op4 =="+" and op5 =="-" and op6 == "*":
+            print(num13 + num23 - num33 * num43)
+        elif op4 =="-" and op5 =="+" and op6 == "+":
+            print(num13 - num23 + num33 + num43)
+        elif op4 =="-" and op5 =="+" and op6 == "-":
+            print(num13 - num23 + num33 - num43)
+        elif op4 =="-" and op5 =="+" and op6 == "/":
+            print(num13 - num23 + num33 / num43)
+        elif op4 =="-" and op5 =="+" and op6 == "*":
+            print(num13 - num23 + num33 * num43)
+        elif op4 =="-" and op5 =="-" and op6 == "+":
+            print(num13 - num23 - num33 + num43)
+        elif op4 =="-" and op5 =="-" and op6 == "-":
+            print(num13 - num23 - num33 - num43)
+        elif op4 =="-" and op5 =="-" and op6 == "/":
+            print(num13 - num23 - num33 / num43)
+        elif op4 =="-" and op5 =="-" and op6 == "*":
+            print(num13 - num23 - num33 * num43)
+        elif op4 =="/" and op5 =="+" and op6 == "+":
+            print(num13 / num23 + num33 + num43)
+        elif op4 =="/" and op5 =="+" and op6 == "-":
+            print(num13 / num23 + num33 - num43)
+        elif op4 =="/" and op5 =="+" and op6 == "/":
+            print(num13 / num23 + num33 / num43)
+        elif op4 =="/" and op5 =="+" and op6 == "*":
+            print(num13 / num23 + num33 * num43)
+        elif op4 =="/" and op5 =="-" and op6 == "+":
+            print(num13 / num23 - num33 + num43)
+        elif op4 =="/" and op5 =="-" and op6 == "-":
+            print(num13 / num23 - num33 - num43)
+        elif op4 =="/" and op5 =="-" and op6 == "/":
+            print(num13 / num23 - num33 / num43)
+        elif op4 =="/" and op5 =="-" and op6 == "*":
+            print(num13 / num23 - num33 * num43)
+        elif op4 =="*" and op5 =="+" and op6 == "+":
+            print(num13 * num23 + num33 + num43)
+        elif op4 =="*" and op5 =="+" and op6 == "-":
+            print(num13 * num23 + num33 - num43)
+        elif op4 =="*" and op5 =="+" and op6 == "/":
+            print(num13 * num23 + num33 / num43)
+        elif op4 =="*" and op5 =="+" and op6 == "*":
+            print(num13 * num23 + num33 * num43)
+        elif op4 =="*" and op5 =="-" and op6 == "+":
+            print(num13 * num23 - num33 + num43)
+        elif op4 =="*" and op5 =="-" and op6 == "-":
+            print(num13 * num23 - num33 - num43)
+        elif op4 =="*" and op5 =="-" and op6 == "/":
+            print(num13 * num23 - num33 / num43)
+        elif op4 =="*" and op5 =="-" and op6 == "*":
+            print(num13 * num23 - num33 * num43)
+        elif op4 =="+" and op5 =="/" and op6 == "+":
+            print(num13 + num23 / num33 + num43)
+        elif op4 =="+" and op5 =="/" and op6 == "-":
+            print(num13 + num23 + num33 - num43)
+        elif op4 =="+" and op5 =="/" and op6 == "/":
+            print(num13 + num23 / num33 / num43)
+        elif op4 =="+" and op5 =="/" and op6 == "*":
+            print(num13 + num23 / num33 * num43)
+        elif op4 =="+" and op5 =="*" and op6 == "+":
+            print(num13 + num23 * num33 + num43)
+        elif op4 =="+" and op5 =="*" and op6 == "-":
+            print(num13 + num23 * num33 - num43)
+        elif op4 =="+" and op5 =="*" and op6 == "/":
+            print(num13 + num23 - num33 / num43)
+        elif op4 =="+" and op5 =="*" and op6 == "*":
+            print(num13 + num23 - num33 * num43)
+        elif op4 =="-" and op5 =="/" and op6 == "+":
+            print(num13 - num23 / num33 + num43)
+        elif op4 =="-" and op5 =="/" and op6 == "-":
+            print(num13 - num23 / num33 - num43)
+        elif op4 =="-" and op5 =="/" and op6 == "/":
+            print(num13 - num23 / num33 / num43)
+        elif op4 =="-" and op5 =="/" and op6 == "*":
+            print(num13 - num23 / num33 * num43)
+        elif op4 =="-" and op5 =="*" and op6 == "+":
+            print(num13 - num23 * num33 + num43)
+        elif op4 =="-" and op5 =="*" and op6 == "-":
+            print(num13 - num23 * num33 - num43)
+        elif op4 =="-" and op5 =="*" and op6 == "/":
+            print(num13 - num23 * num33 / num43)
+        elif op4 =="-" and op5 =="*" and op6 == "*":
+            print(num13 - num23 * num33 * num43)
+        elif op4 =="/" and op5 =="/" and op6 == "+":
+            print(num13 / num23 / num33 + num43)
+        elif op4 =="/" and op5 =="/" and op6 == "-":
+            print(num13 / num23 / num33 - num43)
+        elif op4 =="/" and op5 =="/" and op6 == "/":
+            print(num13 / num23 / num33 / num43)
+        elif op4 =="/" and op5 =="/" and op6 == "*":
+            print(num13 / num23 / num33 * num43)
+        elif op4 =="/" and op5 =="*" and op6 == "+":
+            print(num13 / num23 * num33 + num43)
+        elif op4 =="/" and op5 =="*" and op6 == "-":
+            print(num13 / num23 * num33 - num43)
+        elif op4 =="/" and op5 =="*" and op6 == "/":
+            print(num13 / num23 - num33 / num43)
+        elif op4 =="/" and op5 =="*" and op6 == "*":
+            print(num13 / num23 * num33 * num43)
+        elif op4 =="*" and op5 =="/" and op6 == "+":
+            print(num13 * num23 / num33 + num43)
+        elif op4 =="*" and op5 =="/" and op6 == "-":
+            print(num13 * num23 / num33 - num43)
+        elif op4 =="*" and op5 =="/" and op6 == "/":
+            print(num13 * num23 / num33 / num43)
+        elif op4 =="*" and op5 =="/" and op6 == "*":
+            print(num13 * num23 / num33 * num43)
+        elif op4 =="*" and op5 =="*" and op6 == "+":
+            print(num13 * num23 * num33 + num43)
+        elif op4 =="*" and op5 =="*" and op6 == "-":
+            print(num13 * num23 * num33 - num43)
+        elif op4 =="*" and op5 =="*" and op6 == "/":
+            print(num13 * num23 * num33 / num43)
+        elif op4 =="*" and op5 =="*" and op6 == "*":
+            print(num13 * num23 * num33 * num43)
+
     
-    cont = input("Press Enter to continue: ")
+cont = input("Press Enter to continue: ")
